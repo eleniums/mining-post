18ccda3387b70f0ee5da91a1d1f13d191f939c63
eleniums <eleniums@yahoo.com>
2023-11-17 11:40:51 -0800

    Add some stock with values

--- game/stock.go
+++ game/stock.go
@@ -1,42 +1,51 @@
 package game
 
+// Get default list of all stock and prices.
 func GetInitialStock() []Listing {
 	stock := []Listing{
-		{
-			Resource: Resource{
-				Name:        "Granite",
-				Description: "Rough hewn igneous rock that is rather durable.",
-			},
-			Quantity:  1000,
-			BuyPrice:  10.00,
-			SellPrice: 8.00,
-		},
 		{
 			Resource: Resource{
 				Name:        "Limestone",
 				Description: "Soft, chalky, sedimentary rock that is easily crushed for use in concrete",
 			},
-			Quantity:  1000,
-			BuyPrice:  4.00,
-			SellPrice: 8.00,
-		},
-		{
-			Resource: Resource{
-				Name:        "Marble",
-				Description: "Metamorphic rock that polishes well and is often used for decorative purposes.",
-			},
-			Quantity:  1000,
-			BuyPrice:  20.00,
-			SellPrice: 8.00,
+			quantityRangeLow:  10,
+			quantityRangeHigh: 1000,
+			buyRangeLow:       5,
+			buyRangeHigh:      20,
+			sellDelta:         4,
 		},
 		{
 			Resource: Resource{
 				Name:        "Sandstone",
 				Description: "Sedimentary rock unsuitable for buildings that is primarily used for concrete.",
 			},
-			Quantity:  1000,
-			BuyPrice:  8.00,
-			SellPrice: 8.00,
+			quantityRangeLow:  10,
+			quantityRangeHigh: 1000,
+			buyRangeLow:       8,
+			buyRangeHigh:      30,
+			sellDelta:         7,
+		},
+		{
+			Resource: Resource{
+				Name:        "Granite",
+				Description: "Rough hewn igneous rock that is rather durable.",
+			},
+			quantityRangeLow:  10,
+			quantityRangeHigh: 1000,
+			buyRangeLow:       5,
+			buyRangeHigh:      50,
+			sellDelta:         4,
+		},
+		{
+			Resource: Resource{
+				Name:        "Marble",
+				Description: "Metamorphic rock that polishes well and is often used for decorative purposes.",
+			},
+			quantityRangeLow:  10,
+			quantityRangeHigh: 750,
+			buyRangeLow:       10,
+			buyRangeHigh:      100,
+			sellDelta:         9,
 		},
 	}
 
