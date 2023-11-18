f38bbe4d0a61276f87b43378256db0bbe94adee9
eleniums <eleniums@yahoo.com>
2023-11-17 22:26:23 -0800

    Add test for buying items

--- tests/market_test.go
+++ tests/market_test.go
@@ -3,12 +3,29 @@ package tests
 import (
 	"testing"
 
+	"github.com/eleniums/mining-post/server"
+
 	assert "github.com/stretchr/testify/require"
 )
 
 func Test_Integration_ListMarketStock_Success(t *testing.T) {
 	// act
 	resp, err := client.ListMarketStock()
+
+	// assert
+	assert.NoError(t, err)
+	assert.NotNil(t, resp)
+}
+
+func Test_Integration_BuyOrder_Success(t *testing.T) {
+	req := server.BuyOrderRequest{
+		PlayerName: "snelson",
+		ItemName:   "Granite",
+		Quantity:   10,
+	}
+
+	// act
+	resp, err := client.BuyOrder(req)
 
 	// assert
 	assert.NoError(t, err)
