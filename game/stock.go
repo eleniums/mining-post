e51dc0f71a3e88596e79d294444fbd9791e139a4
eleniums <eleniums@yahoo.com>
2023-11-20 17:58:22 -0800

    Update stock values

--- game/stock.go
+++ game/stock.go
@@ -46,7 +46,7 @@ func getInitialStock() []*Listing {
 			},
 			buyRangeLow:  25,
 			buyRangeHigh: 90,
-			sellDelta:    9,
+			sellDelta:    20,
 		},
 		{
 			Resource: Resource{
@@ -55,7 +55,7 @@ func getInitialStock() []*Listing {
 			},
 			buyRangeLow:  30,
 			buyRangeHigh: 100,
-			sellDelta:    9,
+			sellDelta:    20,
 		},
 		{
 			Resource: Resource{
@@ -64,89 +64,88 @@ func getInitialStock() []*Listing {
 			},
 			buyRangeLow:  50,
 			buyRangeHigh: 150,
-			sellDelta:    9,
+			sellDelta:    40,
 		},
-		// TODO: adjust prices
 		{
 			Resource: Resource{
 				Name:        "Garnet",
 				Description: "Garnet is a group of minerals with deep red to vibrant green hues, commonly used as gemstones and abrasives.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  300,
+			buyRangeHigh: 600,
+			sellDelta:    200,
 		},
 		{
 			Resource: Resource{
 				Name:        "Sapphire",
 				Description: "Sapphire is a precious gemstone, typically blue in color, composed of corundum and valued for its hardness and luster.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  1000,
+			buyRangeHigh: 3000,
+			sellDelta:    500,
 		},
 		{
 			Resource: Resource{
 				Name:        "Ruby",
 				Description: "Ruby is a red precious gemstone, a variety of corundum, prized for its vibrant color and considered one of the most precious gems.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  1000,
+			buyRangeHigh: 3000,
+			sellDelta:    500,
 		},
 		{
 			Resource: Resource{
 				Name:        "Emerald",
 				Description: "Emerald is a green precious gemstone, a variety of the mineral beryl, celebrated for its vivid color and historical significance.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  1000,
+			buyRangeHigh: 3000,
+			sellDelta:    500,
 		},
 		{
 			Resource: Resource{
 				Name:        "Diamond",
 				Description: "Diamond is a brilliant and exceptionally hard precious gemstone, composed of carbon atoms arranged in a crystal lattice, prized for its rarity and used in various luxury applications.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  6000,
+			buyRangeHigh: 12000,
+			sellDelta:    1000,
 		},
 		{
 			Resource: Resource{
 				Name:        "Copper",
 				Description: "Copper is a reddish-brown metal known for its conductivity and malleability.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  20,
+			buyRangeHigh: 50,
+			sellDelta:    10,
 		},
 		{
 			Resource: Resource{
 				Name:        "Silver",
 				Description: "Silver is a lustrous, precious metal admired for its conductivity and versatile use in jewelry and industry.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  250,
+			buyRangeHigh: 750,
+			sellDelta:    100,
 		},
 		{
 			Resource: Resource{
 				Name:        "Gold",
 				Description: "Gold is a prized, yellow metal celebrated for its rarity, beauty, and historical significance in various cultural and economic contexts.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  5000,
+			buyRangeHigh: 10000,
+			sellDelta:    2000,
 		},
 		{
 			Resource: Resource{
 				Name:        "Platinum",
 				Description: "Platinum is a dense, silvery-white metal valued for its durability and use in jewelry and catalytic converters.",
 			},
-			buyRangeLow:  50,
-			buyRangeHigh: 150,
-			sellDelta:    9,
+			buyRangeLow:  7500,
+			buyRangeHigh: 15000,
+			sellDelta:    2000,
 		},
 	}
 }
