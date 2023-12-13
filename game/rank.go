95777b17e74b78a7c1e215553f3bafaa390a5b07
eleniums <eleniums@yahoo.com>
2023-12-12 17:05:40 -0800

    Add some new ranks

--- game/rank.go
+++ game/rank.go
@@ -9,17 +9,59 @@ type Rank struct {
 
 var ranks = []Rank{
 	{
+		Name:   "Amateur Miner L1",
+		Salary: 10,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 250
+		},
+	},
+	{
+		Name:   "Amateur Miner L2",
+		Salary: 50,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 500
+		},
+	},
+	{
+		Name:   "Amateur Miner L3",
+		Salary: 75,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 1_000
+		},
+	},
+	{
+		Name:   "Apprentice Miner L1",
+		Salary: 100,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 10_000
+		},
+	},
+	{
+		Name:   "Apprentice Miner L2",
+		Salary: 250,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 25_000
+		},
+	},
+	{
+		Name:   "Apprentice Miner L3",
+		Salary: 500,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 50_000
+		},
+	},
+	{
 		Name:   "Miner L1",
 		Salary: 1_000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 50_000
+			return player.Money >= 100_000
 		},
 	},
 	{
 		Name:   "Miner L2",
 		Salary: 5_000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 200_000
+			return player.Money >= 250_000
 		},
 	},
 	{
@@ -47,12 +89,26 @@ var ranks = []Rank{
 		Name:   "Principal Miner",
 		Salary: 250_000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 1_000_000_000
+			return player.Money >= 100_000_000
 		},
 	},
 	{
 		Name:   "Distinguished Miner",
 		Salary: 750_000,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 1_000_000_000
+		},
+	},
+	{
+		Name:   "Legendary Miner",
+		Salary: 1_000_000,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 1_000_000_000_000
+		},
+	},
+	{
+		Name:   "Mythic Miner",
+		Salary: 10_000_000,
 		eligibleForPromotion: func(player *Player) bool {
 			return false
 		},
