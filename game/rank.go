6e03f2548a4d7a01cfa99189921cdd7f9549c912
eleniums <eleniums@yahoo.com>
2023-12-13 17:33:28 -0800

    Adjust rank values again
    They make more sense now, I think. Each set of levels is a tier and the salary goes up predictably. Each tier has more of a jump. Once you get to the named levels, it gets progressively harder to rank up.

--- game/rank.go
+++ game/rank.go
@@ -7,104 +7,103 @@ type Rank struct {
 	eligibleForPromotion func(player *Player) bool
 }
 
-// TODO: make sure salaries and promotions make sense
 var ranks = []Rank{
 	{
 		Name:   "Amateur Miner L1",
 		Salary: 10,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 250
+			return player.Money >= 1000
 		},
 	},
 	{
 		Name:   "Amateur Miner L2",
 		Salary: 50,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 500
+			return player.Money >= 5000
 		},
 	},
 	{
 		Name:   "Amateur Miner L3",
-		Salary: 75,
-		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 1_000
-		},
-	},
-	{
-		Name:   "Apprentice Miner L1",
 		Salary: 100,
 		eligibleForPromotion: func(player *Player) bool {
 			return player.Money >= 10_000
 		},
 	},
 	{
-		Name:   "Apprentice Miner L2",
-		Salary: 250,
-		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 25_000
-		},
-	},
-	{
-		Name:   "Apprentice Miner L3",
+		Name:   "Apprentice Miner L1",
 		Salary: 500,
 		eligibleForPromotion: func(player *Player) bool {
 			return player.Money >= 50_000
 		},
 	},
 	{
-		Name:   "Miner L1",
-		Salary: 1_000,
+		Name:   "Apprentice Miner L2",
+		Salary: 1000,
 		eligibleForPromotion: func(player *Player) bool {
 			return player.Money >= 100_000
 		},
 	},
 	{
-		Name:   "Miner L2",
-		Salary: 5_000,
+		Name:   "Apprentice Miner L3",
+		Salary: 2000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 250_000
+			return player.Money >= 200_000
 		},
 	},
 	{
-		Name:   "Miner L3",
+		Name:   "Miner L1",
 		Salary: 10_000,
-		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 500_000
-		},
-	},
-	{
-		Name:   "Senior Miner",
-		Salary: 30_000,
 		eligibleForPromotion: func(player *Player) bool {
 			return player.Money >= 1_000_000
 		},
 	},
 	{
-		Name:   "Staff Miner",
+		Name:   "Miner L2",
+		Salary: 20_000,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 2_000_000
+		},
+	},
+	{
+		Name:   "Miner L3",
+		Salary: 40_000,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 4_000_000
+		},
+	},
+	{
+		Name:   "Senior Miner",
 		Salary: 100_000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 10_000_000
+			return player.Money >= 20_000_000
+		},
+	},
+	{
+		Name:   "Staff Miner",
+		Salary: 250_000,
+		eligibleForPromotion: func(player *Player) bool {
+			return player.Money >= 50_000_000
 		},
 	},
 	{
 		Name:   "Principal Miner",
-		Salary: 250_000,
+		Salary: 500_000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 100_000_000
+			return player.Money >= 1_000_000_000
 		},
 	},
 	{
 		Name:   "Distinguished Miner",
 		Salary: 750_000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 1_000_000_000
+			return player.Money >= 1_000_000_000_000
 		},
 	},
 	{
 		Name:   "Legendary Miner",
 		Salary: 1_000_000,
 		eligibleForPromotion: func(player *Player) bool {
-			return player.Money >= 1_000_000_000_000
+			return player.Money >= 100_000_000_000_000
 		},
 	},
 	{
