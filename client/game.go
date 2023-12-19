2b2447b338921eef6232bc3905a2e8480b91f009
eleniums <eleniums@yahoo.com>
2023-12-18 17:23:11 -0800

    Simplify client response handling

--- client/game.go
+++ client/game.go
@@ -27,42 +27,15 @@ func NewGameClient(url string) *GameClient {
 }
 
 func (c *GameClient) GameInfo() (*server.GameInfoResponse, error) {
-	code, body, err := c.client.Get(fmt.Sprintf("%s/game/info", c.rootURL))
-	if err != nil {
-		return nil, fmt.Errorf("error calling service: %v", err)
-	}
-	if code != http.StatusOK {
-		return nil, fmt.Errorf("invalid status code: %d, message: %v", code, string(body))
-	}
-
-	var resp server.GameInfoResponse
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling response body: %v", err)
-	}
-
-	return &resp, nil
+	return handleClientResp[server.GameInfoResponse](c.client.Get(fmt.Sprintf("%s/game/info", c.rootURL)))
 }
 
-func (c *GameClient) GetPlayerInventory(name string) (server.GetPlayerInventoryResponse, error) {
-	code, body, err := c.client.Get(fmt.Sprintf("%s/player/%s/inventory", c.rootURL, name))
-	if err != nil {
-		return server.GetPlayerInventoryResponse{}, fmt.Errorf("error calling service: %v", err)
-	}
-	if code != http.StatusOK {
-		return server.GetPlayerInventoryResponse{}, fmt.Errorf("invalid status code: %d, message: %v", code, string(body))
-	}
+func (c *GameClient) GetPlayerInventory(name string) (*server.GetPlayerInventoryResponse, error) {
+	return handleClientResp[server.GetPlayerInventoryResponse](c.client.Get(fmt.Sprintf("%s/player/%s/inventory", c.rootURL, name)))
 
-	var resp server.GetPlayerInventoryResponse
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		return server.GetPlayerInventoryResponse{}, fmt.Errorf("error unmarshaling response body: %v", err)
-	}
-
-	return resp, nil
 }
 
-func (c *GameClient) ListMarketStock(filters ...Filter) (server.ListMarketStockResponse, error) {
+func (c *GameClient) ListMarketStock(filters ...Filter) (*server.ListMarketStockResponse, error) {
 	queryParams := []string{}
 
 	filterParam := []string{}
@@ -73,65 +46,40 @@ func (c *GameClient) ListMarketStock(filters ...Filter) (server.ListMarketStockR
 		queryParams = append(queryParams, "filter", strings.Join(filterParam, ","))
 	}
 
-	code, body, err := c.client.Get(fmt.Sprintf("%s/market/stock", c.rootURL), queryParams...)
-	if err != nil {
-		return server.ListMarketStockResponse{}, fmt.Errorf("error calling service: %v", err)
-	}
-	if code != http.StatusOK {
-		return server.ListMarketStockResponse{}, fmt.Errorf("invalid status code: %d, message: %v", code, string(body))
-	}
-
-	var resp server.ListMarketStockResponse
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		return server.ListMarketStockResponse{}, fmt.Errorf("error unmarshaling response body: %v", err)
-	}
-
-	return resp, nil
+	return handleClientResp[server.ListMarketStockResponse](c.client.Get(fmt.Sprintf("%s/market/stock", c.rootURL), queryParams...))
 }
 
-func (c *GameClient) BuyOrder(req server.BuyOrderRequest) (server.BuyOrderResponse, error) {
+func (c *GameClient) BuyOrder(req server.BuyOrderRequest) (*server.BuyOrderResponse, error) {
 	payload, err := json.Marshal(req)
 	if err != nil {
-		return server.BuyOrderResponse{}, fmt.Errorf("error marshaling request: %v", err)
+		return nil, fmt.Errorf("error marshaling request: %v", err)
 	}
 
-	code, body, err := c.client.Post(fmt.Sprintf("%s/market/buy", c.rootURL), payload)
-	if err != nil {
-		return server.BuyOrderResponse{}, fmt.Errorf("error calling service: %v", err)
-	}
-	if code != http.StatusOK {
-		return server.BuyOrderResponse{}, fmt.Errorf("invalid status code: %d, message: %v", code, string(body))
-	}
-
-	var resp server.BuyOrderResponse
-	err = json.Unmarshal(body, &resp)
-	if err != nil {
-		return server.BuyOrderResponse{}, fmt.Errorf("error unmarshaling response body: %v", err)
-	}
-
-	return resp, nil
+	return handleClientResp[server.BuyOrderResponse](c.client.Post(fmt.Sprintf("%s/market/buy", c.rootURL), payload))
 }
 
-func (c *GameClient) SellOrder(req server.SellOrderRequest) (server.SellOrderResponse, error) {
+func (c *GameClient) SellOrder(req server.SellOrderRequest) (*server.SellOrderResponse, error) {
 	payload, err := json.Marshal(req)
 	if err != nil {
-		return server.SellOrderResponse{}, fmt.Errorf("error marshaling request: %v", err)
+		return nil, fmt.Errorf("error marshaling request: %v", err)
 	}
 
-	code, body, err := c.client.Post(fmt.Sprintf("%s/market/sell", c.rootURL), payload)
+	return handleClientResp[server.SellOrderResponse](c.client.Post(fmt.Sprintf("%s/market/sell", c.rootURL), payload))
+}
+
+func handleClientResp[T any](code int, body []byte, err error) (*T, error) {
 	if err != nil {
-		return server.SellOrderResponse{}, fmt.Errorf("error calling service: %v", err)
+		return nil, fmt.Errorf("error calling service: %v", err)
 	}
 	if code != http.StatusOK {
-		return server.SellOrderResponse{}, fmt.Errorf("invalid status code: %d, message: %v", code, string(body))
+		return nil, fmt.Errorf("invalid status code: %d, message: %v", code, string(body))
 	}
 
-	var resp server.SellOrderResponse
+	var resp T
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
-		return server.SellOrderResponse{}, fmt.Errorf("error unmarshaling response body: %v", err)
+		return nil, fmt.Errorf("error unmarshaling response body: %v", err)
 	}
 
-	return resp, nil
+	return &resp, nil
 }
