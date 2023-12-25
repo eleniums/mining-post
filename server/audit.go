d2e8513934d0aa7df68e2f29e7b74041566e428d
eleniums <eleniums@yahoo.com>
2023-12-25 08:19:43 -0800

    Add response code to log

--- server/audit.go
+++ server/audit.go
@@ -4,19 +4,27 @@ import (
 	"log/slog"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/middleware"
 )
 
 // Audit interceptor to log method calls.
 func Audit(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		// execute method
 		start := time.Now()
-		next.ServeHTTP(w, req)
+
+		// wrap the ResponseWriter so we can retrieve the response status
+		ww := middleware.NewWrapResponseWriter(w, req.ProtoMajor)
+
+		// execute method
+		next.ServeHTTP(ww, req)
 
 		// log completion and time elapsed
 		slog.Info("Handled request",
 			"verb", req.Method,
 			"method", req.URL.Path,
-			"elapsed", time.Since(start))
+			"elapsed", time.Since(start),
+			"code", ww.Status(),
+		)
 	})
 }
