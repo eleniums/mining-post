e6c5dd16da45f3d99c561bd4c62856d26518e19b
eleniums <eleniums@yahoo.com>
2023-12-30 14:15:23 -0800

    Rename some variables yet again

--- cmd/main.go
+++ cmd/main.go
@@ -34,7 +34,7 @@ var (
 var (
 	httpHost  string
 	httpPort  string
-	dbPath    string
+	dbConn    string
 	certFile  string
 	keyFile   string
 	logLevel  string
@@ -45,7 +45,7 @@ func main() {
 	// parse configuration - flags have highest priority, then env vars, and then defaults
 	flag.StringVar(&httpHost, "http-host", getEnvStr("HTTP_HOST", "127.0.0.1"), "HTTP_HOST: host to serve endpoint on")
 	flag.StringVar(&httpPort, "http-port", getEnvStr("HTTP_PORT", "9090"), "HTTP_PORT: port to serve endpoint on")
-	flag.StringVar(&dbPath, "db-path", getEnvStr("DB_PATH", "game.db"), "DB_PATH: full path to database file")
+	flag.StringVar(&dbConn, "db-conn", getEnvStr("DB_CONN", "game.db"), "DB_CONN: connection string for database")
 	flag.StringVar(&certFile, "tls-cert-file", getEnvStr("TLS_CERT_FILE", ""), "TLS_CERT_FILE: cert file for enabling a TLS connection")
 	flag.StringVar(&keyFile, "tls-key-file", getEnvStr("TLS_KEY_FILE", ""), "TLS_KEY_FILE: key file for enabling a TLS connection")
 	flag.StringVar(&logLevel, "log-level", getEnvStr("LOG_LEVEL", "info"), "LOG_LEVEL: level to use for logs (debug|info|warn|error)")
@@ -65,7 +65,7 @@ func main() {
 
 	// open a connection to the database
 	db := data.NewBoltDB()
-	err := db.Open(dbPath)
+	err := db.Open(dbConn)
 	if err != nil {
 		slog.Error("Failed to open database", game.ErrAttr(err))
 		os.Exit(1)
