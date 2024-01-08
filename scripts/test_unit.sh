addf791d21676bdfbdb9b2703fb271aac8a75d51
eleniums <eleniums@yahoo.com>
2024-01-08 15:46:22 -0800

    Exclude integration tests when running unit test script

--- scripts/test_unit.sh
+++ scripts/test_unit.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-# TODO: fix unit test script to only run unit tests, not integration tests. Use grep
-go test -count=1 -cover ./... $@
+go test $(go list ./... | grep -v tests) -count=1 -cover $@
